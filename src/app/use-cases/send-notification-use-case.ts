2611739b638ecf70b80cd68e23cdcb804836ed73
Gabriel Borel <biel_borel@hotmail.com>
2022-12-14 01:52:15 -0300

    send-notification use-case created

---
+++ src/app/use-cases/send-notification-use-case.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { Content } from '../entities/content';
+import { Notification } from '../entities/notification';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+
+interface SendNotificationRequest {
+  recipientId: string;
+  category: string;
+  content: string;
+}
+
+interface SendNotificationResponse {
+  notification: Notification;
+}
+
+@Injectable()
+export class SendNotificationUseCase {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute(
+    request: SendNotificationRequest,
+  ): Promise<SendNotificationResponse> {
+    const { category, content, recipientId } = request;
+
+    const notification = new Notification({
+      recipientId,
+      content: new Content(content),
+      category,
+    });
+
+    await this.notificationsRepository.create(notification);
+
+    return { notification };
+  }
+}

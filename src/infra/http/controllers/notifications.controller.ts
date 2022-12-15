4f9734244a496e07bc70afc8cde61c1fc18d363d
Gabriel Borel <biel_borel@hotmail.com>
2022-12-15 09:20:12 -0300

    read notification route added

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -3,17 +3,24 @@ import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
 import { SendNotificationUseCase } from 'src/app/use-cases/send-notification-use-case';
 import { NotificationViewModel } from '../view-models/notification-view-model';
 import { CancelNotificationUseCase } from '@app/use-cases/cancel-notification-use-case';
+import { ReadNotificationUseCase } from '@app/use-cases/read-notification-use-case';
 
 @Controller('notifications')
 export class NotificationsController {
   constructor(
     private sendNotificationUseCase: SendNotificationUseCase,
     private cancelNotificationUseCase: CancelNotificationUseCase,
+    private readNotificationUseCase: ReadNotificationUseCase,
   ) {}
 
   @Patch(':id/cancel')
   async cancel(@Param('id') id: string) {
     await this.cancelNotificationUseCase.execute({ notificationId: id });
+  }
+
+  @Patch(':id/read')
+  async read(@Param('id') id: string) {
+    await this.readNotificationUseCase.execute({ notificationId: id });
   }
 
   @Post()

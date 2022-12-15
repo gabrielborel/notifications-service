4015540e4706bd22cd4ca2954dac36966d4f1876
Gabriel Borel <biel_borel@hotmail.com>
2022-12-15 09:10:40 -0300

    implement all method on in memory notifications repository

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -6,7 +6,26 @@ export class InMemoryNotificationsRepository
 {
   public notifications: Notification[] = [];
 
+  async findById(notificationId: string): Promise<Notification | null> {
+    return this.notifications.find((n) => n.id === notificationId) ?? null;
+  }
+
+  async save(notification: Notification): Promise<void> {
+    this.notifications.map((n) =>
+      n.id === notification.id ? notification : n,
+    );
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification);
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    return this.notifications.filter((n) => n.recipientId === recipientId)
+      .length;
+  }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter((n) => n.recipientId === recipientId);
   }
 }

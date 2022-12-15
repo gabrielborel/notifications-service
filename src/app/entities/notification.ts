67227a9f33cad65482c5d2781b6627c297623512
Gabriel Borel <biel_borel@hotmail.com>
2022-12-15 08:54:12 -0300

    added canceledAt propety and cancel method on notification entity

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -7,6 +7,7 @@ export interface NotificationProps {
   content: Content;
   category: string;
   readAt?: Date | null;
+  canceledAt?: Date | null;
   createdAt: Date;
 }
 
@@ -14,12 +15,19 @@ export class Notification {
   private _id: string;
   private props: NotificationProps;
 
-  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
-    this._id = randomUUID();
+  constructor(
+    props: Replace<NotificationProps, { createdAt?: Date }>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
     };
+  }
+
+  public cancel(): void {
+    this.props.canceledAt = new Date();
   }
 
   public get id(): string {
@@ -56,6 +64,10 @@ export class Notification {
 
   public get readAt(): Date | null | undefined {
     return this.props.readAt;
+  }
+
+  public get canceledAt(): Date | null | undefined {
+    return this.props.canceledAt;
   }
 
   public get createdAt(): Date {

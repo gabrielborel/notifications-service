fc79a4b5e79c333be30dc7ce0c39c5c66bf75541
Gabriel Borel <biel_borel@hotmail.com>
2022-12-15 08:58:59 -0300

    prisma notification mapper created with methods toPrisma and toDomain

---
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -0,0 +1,30 @@
+import { Content } from '@app/entities/content';
+import { Notification } from '@app/entities/notification';
+import { Notification as PrismaNotification } from '@prisma/client';
+
+export class PrismaNotificationMapper {
+  static toPrisma(notification: Notification) {
+    return {
+      id: notification.id,
+      content: notification.content.value,
+      category: notification.category,
+      recipientId: notification.recipientId,
+      readAt: notification.readAt,
+      createdAt: notification.createdAt,
+    };
+  }
+
+  static toDomain(raw: PrismaNotification) {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+        readAt: raw.readAt,
+      },
+      raw.id,
+    );
+  }
+}

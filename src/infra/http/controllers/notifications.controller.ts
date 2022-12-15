9baeee94ea80cedbe93ede53a9332f21b3438756
Gabriel Borel <biel_borel@hotmail.com>
2022-12-15 11:16:52 -0300

    get notifications by recipient id route added

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,11 +1,12 @@
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { CountRecipientNotificationsUseCase } from './../../../app/use-cases/count-recipient-notifications-use-case';
 import { CreateNotificationDTO } from '../dtos/create-notification.dto';
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { SendNotificationUseCase } from 'src/app/use-cases/send-notification-use-case';
 import { NotificationViewModel } from '../view-models/notification-view-model';
 import { CancelNotificationUseCase } from '@app/use-cases/cancel-notification-use-case';
 import { ReadNotificationUseCase } from '@app/use-cases/read-notification-use-case';
 import { UnreadNotificationUseCase } from '@app/use-cases/unread-notification-use-case';
+import { GetRecipientNotificationsUseCase } from '@app/use-cases/get-recipient-notifications-use-case';
 
 @Controller('notifications')
 export class NotificationsController {
@@ -15,6 +16,7 @@ export class NotificationsController {
     private readNotificationUseCase: ReadNotificationUseCase,
     private unreadNotificationUseCase: UnreadNotificationUseCase,
     private countRecipientNotificationsUseCase: CountRecipientNotificationsUseCase,
+    private getRecipientNotificationsUseCase: GetRecipientNotificationsUseCase,
   ) {}
 
   @Patch(':id/cancel')
@@ -54,5 +56,15 @@ export class NotificationsController {
     });
 
     return { count };
+  }
+
+  @Get('/from/:recipientId')
+  async getFromRecipient(@Param('recipientId') id: string) {
+    const { notifications } =
+      await this.getRecipientNotificationsUseCase.execute({
+        recipientId: id,
+      });
+
+    return { notifications: notifications.map(NotificationViewModel.toHTTP) };
   }
 }
